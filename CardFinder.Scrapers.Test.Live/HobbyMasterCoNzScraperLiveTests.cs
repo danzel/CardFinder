96874e151a2a46bd03351052ce97f020082ca8d7
Dave Leaver <dave@torutek.com>
2023-05-04 11:39:56 +1200

    Hobby Master live test

---
+++ CardFinder.Scrapers.Test.Live/HobbyMasterCoNzScraperLiveTests.cs
@@ -0,0 +1,18 @@
+﻿using CardFinder.Scrapers.Helpers;
+using CardFinder.Scrapers.SingleSite;
+using Microsoft.Extensions.Logging;
+
+namespace CardFinder.Scrapers.Test.Live;
+public class HobbyMasterCoNzScraperLiveTests : LiveTestsBase
+{
+	[Fact]
+	public async Task GetAridMesa()
+	{
+		var scraper = new HobbyMasterCoNzScraper(Logger.CreateLogger<HobbyMasterCoNzScraper>(), DirectHttpClient, new DefaultConditionParser(), new HobbyMasterTreatmentParser());
+
+		var cards = await scraper.Scrape("Arid Mesa");
+
+		Assert.NotEmpty(cards);
+		Console.WriteLine($"Found {cards.Length} cards");
+	}
+}
